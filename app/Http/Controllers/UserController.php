dc6439799b2f4ac13350b3a48c9a94d0964c12a8
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-07-16 10:37:02 +0000

    Add user create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Client;
+use App\Office;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -18,7 +20,7 @@ class UserController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Show the user list.
      *
      * @return \Illuminate\Http\Response
      */
@@ -27,5 +29,76 @@ class UserController extends Controller
         $users = User::with(['client', 'office'])->get();
 
         return view('users')->with('users', $users);
+    }
+
+    /**
+     * Show the form for creating a new user.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $clients = Client::pluck('name', 'id');
+        $offices = Office::pluck('city', 'id');
+
+        return view('users.create')->with(['clients' => $clients, 'offices' => $offices]);
+    }
+
+    /**
+     * Store a newly created user in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'position' => 'required|string|max:255',
+            'birthday' => 'required|date',
+            'avatar' => 'required|url',
+            'client_id' => 'required|exists:clients,id',
+            'office_id' => 'required|exists:offices,id',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        User::create(request()->all());
+
+        return back()->with('status', 'The user was successfully created!');
+    }
+
+    /**
+     * Show the form for editing the specified user.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        
+    }
+
+    /**
+     * Update the specified user in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        
+    }
+
+    /**
+     * Remove the specified user from storage.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/users', 'UserController@index')->name('users');
-
 Route::get('/clients', 'ClientController@index')->name('clients');
-
 Route::get('/offices', 'OfficeController@index')->name('offices');
+
+Route::prefix('admin')->group(function () {
+    Route::resource('users', 'UserController', ['except' => ['index', 'show']]);
+    Route::resource('clients', 'ClientController', ['except' => ['index', 'show']]);
+    Route::resource('offices', 'OfficeController', ['except' => ['index', 'show']]);
+});
