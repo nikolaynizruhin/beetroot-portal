11bf4ec4c9749051250bdd14398a0193be61b476
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-09-11 21:22:03 +0000

    Add test

--- app/Http/Controllers/UserPasswordController.php
+++ app/Http/Controllers/UserPasswordController.php
@@ -9,6 +9,16 @@ use App\User;
 class UserPasswordController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Update user password.
      *
      * @param  \App\Http\Requests\UpdateUserPassword  $request
---
+++ tests/Feature/UpdateUserPasswordTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UpdateUserPasswordTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Only user can update own password.
+     *
+     * @return void
+     */
+    public function testOnlyUserCanUpdateOwnPassword()
+    {
+        $owner = factory(User::class)->create();
+        $user = factory(User::class)->create(['is_admin' => false]);
+
+        $this->put(route('users.password.update', $owner->id))
+            ->assertRedirect('login');
+
+        $this->actingAs($user)
+            ->put(route('users.password.update', $owner->id))
+            ->assertStatus(403);
+    }
+
+    /**
+     * Admin can update user password.
+     *
+     * @return void
+     */
+    public function testAdminCanUpdateUserPassword()
+    {
+        $owner = factory(User::class)->create();
+        $admin = factory(User::class)->states('admin')->create();
+
+        $attributes = [
+            'password' => 'secret-updated',
+            'password_confirmation' => 'secret-updated',
+        ];
+
+        $this->actingAs($admin)
+            ->put(route('users.password.update', $owner->id), $attributes)
+            ->assertSessionHas('status', 'The user password was successfully updated!');
+
+        $owner = $owner->fresh();
+
+        $this->assertTrue(Hash::check('secret-updated', $owner->password));
+    }
+
+    /**
+     * User can update own password.
+     *
+     * @return void
+     */
+    public function testUserCanUpdateOwnPassword()
+    {
+        $owner = factory(User::class)->create(['is_admin' => false]);
+
+        $attributes = [
+            'password' => 'secret-updated',
+            'password_confirmation' => 'secret-updated',
+        ];
+
+        $this->actingAs($owner)
+            ->put(route('users.password.update', $owner->id), $attributes)
+            ->assertSessionHas('status', 'The user password was successfully updated!');
+
+        $owner = $owner->fresh();
+
+        $this->assertTrue(Hash::check('secret-updated', $owner->password));
+    }
+
+    /**
+     * User password fields are required.
+     *
+     * @return void
+     */
+    public function testUserPasswordFieldsAreRequired()
+    {
+        $owner = factory(User::class)->create();
+
+        $this->actingAs($owner)
+            ->put(route('users.password.update', $owner->id))
+            ->assertSessionHasErrors(['password']);
+    }
+}
