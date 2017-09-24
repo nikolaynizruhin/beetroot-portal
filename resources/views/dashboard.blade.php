984662287d929f0f440224ddf8d6e0ccaddf9d30
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-09-24 14:55:09 +0000

    Update design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                 <div class="panel-body">
                     <div class="row text-center">
                         <div class="col-md-4">
-                            <p>TOTAL USERS</p>
+                            <p>TOTAL EMPLOYEES</p>
                             <number-counter :count="{{ $userCount }}"></number-counter>
                         </div>
                         <div class="col-md-4">
@@ -27,7 +27,7 @@
                         </div>
                     </div>
                     <hr>
-                    <h3 class="text-center">POSITIONS</h3>
+                    <h3 class="text-center">TOP 5 POSITIONS</h3>
                     <hr>
                     <div class="row">
                         <div class="col-md-12">
