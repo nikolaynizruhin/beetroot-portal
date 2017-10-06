f88cf600834c0227f38ec4b0a726203349bbc7e6
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-10-06 19:15:54 +0000

    Disable registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -40,6 +40,27 @@ class RegisterController extends Controller
     }
 
     /**
+     * Show the application registration form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showRegistrationForm()
+    {
+        return redirect('login');
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+
+    }
+
+    /**
      * Get a validator for an incoming registration request.
      *
      * @param  array  $data
