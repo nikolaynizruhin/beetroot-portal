f517c222a3528e91eda4390afa31cb75c5ab9307
nikolaynizruhin <lgnexus4fun@gmail.com>
2017-11-23 17:50:40 +0000

    Update title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Beetroot</title>
+    <title>Beetroot Family Portal</title>
 
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/ico" href="{{ asset('favicon.ico') }}"/>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Beetroot</title>
+        <title>Beetroot Family Portal</title>
 
         <!-- Favicon -->
         <link rel="shortcut icon" type="image/ico" href="favicon.ico"/>
