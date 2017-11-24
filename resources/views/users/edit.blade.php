47ca131e319656a6a2fb1fae93303994e3c10596
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-11-24 14:48:40 +0200

    Update delete button design

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -246,7 +246,7 @@
                             </div>
                             @admin
                                 <div class="col-md-3 mt-1">
-                                    <button type="submit" class="btn btn-danger btn-block"
+                                    <button type="submit" class="btn btn-default btn-block"
                                             onclick="event.preventDefault();if(confirm('Are you sure you want to delete an employee?'))document.getElementById('delete-form').submit();">
                                         Delete
                                     </button>
