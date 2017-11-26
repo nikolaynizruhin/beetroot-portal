68368c4294e77ee3de2eddc2249781eb45bf0d94
nikolaynizruhin <lgnexus4fun@gmail.com>
2017-11-26 12:42:05 +0000

    Fix admin nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -52,17 +52,19 @@
                             @endrouteis
                         </a>
                     </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                            Admin
-                            <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="{{ route('users.create') }}">Add Employee</a></li>
-                            <li><a href="{{ route('clients.create') }}">Add Client</a></li>
-                            <li><a href="{{ route('offices.create') }}">Add Office</a></li>
-                        </ul>
-                    </li>
+                    @admin
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                Admin
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ route('users.create') }}">Add Employee</a></li>
+                                <li><a href="{{ route('clients.create') }}">Add Client</a></li>
+                                <li><a href="{{ route('offices.create') }}">Add Office</a></li>
+                            </ul>
+                        </li>
+                    @endadmin
                 @endauth
             </ul>
 
