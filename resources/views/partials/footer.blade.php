2116562249ca255a308883a63a35aed651317355
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-11-28 17:21:14 +0200

    Update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
     <div class="container">
-        <p class="text-muted text-center">© {{ date('Y') }} Beetroot. All Rights Reserved.</p>
+        <p class="text-muted text-center">© {{ date('Y') }} {{ config('app.name') }}. All Rights Reserved.</p>
     </div>
 </footer>
