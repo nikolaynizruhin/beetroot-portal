a2bd40c7c1bcf57effa8875fdfe0596a6ce7a61b
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-11-29 11:16:10 +0200

    Update email logo to png

--- resources/views/vendor/mail/markdown/message.blade.php
+++ resources/views/vendor/mail/markdown/message.blade.php
@@ -2,7 +2,7 @@
     {{-- Header --}}
     @slot('header')
         @component('mail::header', ['url' => config('app.url')])
-            ![{{ config('app.name') }}]({{ asset('images/logo.svg') }} "{{ config('app.name') }}")
+            ![{{ config('app.name') }}]({{ asset('images/logo.png') }} "{{ config('app.name') }}")
         @endcomponent
     @endslot
 
