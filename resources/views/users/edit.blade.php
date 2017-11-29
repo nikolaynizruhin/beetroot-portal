984a8406d1a5a2a3116a0912a3200ff01c4c845a
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-11-29 12:40:59 +0200

    Fix autofocus

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -62,7 +62,7 @@
                             <label for="name" class="col-md-4 control-label">Name <small>*</small></label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name', $user->name) }}" required autofocus>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name', $user->name) }}" required @if ($errors->isEmpty()) autofocus @endif>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
