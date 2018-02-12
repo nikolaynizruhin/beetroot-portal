92aeaf4ae3d463c005b544e5bcec0d5a11937442
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2018-02-12 16:52:17 +0200

    Cleanup

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -6,7 +6,13 @@
             <img src="{{ asset('images/logo.svg') }}" height="40" width="112" alt="Beetroot">
         </a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
