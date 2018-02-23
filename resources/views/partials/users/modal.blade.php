87548ff58b733a96ede87c861f595d32f7e10237
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2018-02-23 17:45:58 +0200

    Fix modal ui

--- resources/views/partials/users/modal.blade.php
+++ resources/views/partials/users/modal.blade.php
@@ -116,7 +116,7 @@
                     <!-- Bio -->
                     @if ($user->bio)
                         <div class="row">
-                            <div class="col-sm-10 col-sm-offset-1">
+                            <div class="col">
                                 <p>{{ $user->bio }}</p>
                             </div>
                         </div>
