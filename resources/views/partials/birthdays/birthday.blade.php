6947d9db30a79c0cf1ec18041474c7d9ba2410d4
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2018-02-25 12:59:03 +0000

    Add avatar thumbnail

--- resources/views/partials/birthdays/birthday.blade.php
+++ resources/views/partials/birthdays/birthday.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-4">
     <div class="media my-1">
-        <img class="mr-3 rounded-circle" src="{{ asset('storage/'.$user->avatar) }}" alt="Avatar" width="64" height="64">
+        <img class="mr-3 rounded-circle img-thumbnail" src="{{ asset('storage/'.$user->avatar) }}" alt="Avatar" width="64" height="64">
         <div class="media-body">
             <h5 class="mt-0">{{ $user->birthday->format('j F') }}</h5>
             {{ $user->name }}
