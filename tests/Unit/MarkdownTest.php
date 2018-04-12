dd855c0dbea0ab256f37bb6b110cd4fbd2bf30d5
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2018-04-12 21:29:00 +0000

    Add markdown test

---
+++ tests/Unit/MarkdownTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Http\Utilities\Markdown;
+
+class MarkdownTest extends TestCase
+{
+    /** @test */
+    public function it_parse_markdown()
+    {
+        $html = Markdown::parse('Hello _Markdown_!');
+
+        $this->assertEquals($html, 'Hello <em>Markdown</em>!');
+    }
+}
