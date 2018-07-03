a7402f4766e24fe7defa75ba3066c76b0d3fd871
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2018-07-03 13:57:52 +0300

    Typo

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -32,7 +32,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-mb-4">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     Send Password Reset Link
                                 </button>
