0ec784a2b3e62b1d86b8898ceefa2ff7a7edd0fb
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2018-07-31 08:01:18 +0000

    Remove force on main img

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,7 @@
             }
 
             .background-image {
-                background-image: url("{{ asset('images/welcome.svg', true) }}");
+                background-image: url("{{ asset('images/welcome.svg') }}");
                 height: 100%;
                 background-position: center;
                 background-repeat: no-repeat;
