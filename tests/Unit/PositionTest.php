e9a8aad69b608b860141823971cb606813682641
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2018-08-02 16:25:03 +0300

    Update position test

--- tests/Unit/PositionTest.php
+++ tests/Unit/PositionTest.php
@@ -12,7 +12,7 @@ class PositionTest extends TestCase
     {
         $positions = Position::all();
 
-        $this->assertEquals(count($positions), 46);
+        $this->assertEquals(count($positions), 47);
         $this->assertEquals($positions[0], 'Administration');
     }
 }
