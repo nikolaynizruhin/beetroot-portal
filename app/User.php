fc284c266aabdb67483b46263c9db607de034295
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2018-10-12 11:07:45 +0300

    Style fix

--- app/User.php
+++ app/User.php
@@ -164,7 +164,7 @@ class User extends Authenticatable
     {
         try {
             $this->notify(new WelcomeNotification($password));
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             logger('Unable to send welcome notification');
         }
     }
