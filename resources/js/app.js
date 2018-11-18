4bb657bf89417b563a72fee56c82ce22438bc931
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2018-11-18 02:38:17 +0200

    Add auto register vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,9 +10,11 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
 Vue.component('positions-chart', require('./components/PositionsChart.vue'));
@@ -21,6 +23,15 @@ Vue.component('clients-map', require('./components/ClientsMap.vue'));
 Vue.component('offices-map', require('./components/OfficesMap.vue'));
 Vue.component('grow-chart', require('./components/GrowChart.vue'));
 Vue.component('counter', require('./components/Counter.vue'));
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
 
 const app = new Vue({
     el: '#app'
