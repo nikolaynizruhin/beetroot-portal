0d694bde9534977962c55935268f2c6c4fc4c433
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2019-01-22 21:32:40 +0200

    Cleanup model factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,12 +26,8 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'is_admin' => $faker->boolean,
         'remember_token' => str_random(10),
-        'client_id' => function () {
-            return factory(App\Client::class)->create()->id;
-        },
-        'office_id' => function () {
-            return factory(App\Office::class)->create()->id;
-        },
+        'client_id' => factory(App\Client::class),
+        'office_id' => factory(App\Office::class),
     ];
 });
 
