3c3cfdd1e2d16beaec0c7429e9a05f51889a2ae4
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2019-04-26 17:58:16 +0000

    Apply fixes from StyleCI

--- database/factories/OfficeFactory.php
+++ database/factories/OfficeFactory.php
@@ -1,7 +1,6 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-
 use App\Office;
 use Faker\Generator as Faker;
 use App\Http\Utilities\Country;
--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -1,7 +1,6 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-
 use App\Tag;
 use Faker\Generator as Faker;
 
