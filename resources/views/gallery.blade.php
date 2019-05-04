8057d0a008350385aa37c08e4ee04ad70af907fb
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2019-05-04 14:01:53 +0300

    Update gallery images path

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -24,31 +24,31 @@
                             </ol>
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <img src="{{ asset('images/gallery/birthdays/2_years_2014.jpg') }}" class="d-block w-100" alt="Beetroot Birthday Party">
+                                    <img src="{{ asset('images/gallery/2_years_2014.jpg') }}" class="d-block w-100" alt="Beetroot Birthday Party">
                                     <div class="carousel-caption d-none d-md-block">
                                         <h5>2 Years, 2014</h5>
                                     </div>
                                 </div>
                                 <div class="carousel-item">
-                                    <img src="{{ asset('images/gallery/birthdays/3_years_2015.jpg') }}" class="d-block w-100" alt="Beetroot Birthday Party">
+                                    <img src="{{ asset('images/gallery/3_years_2015.jpg') }}" class="d-block w-100" alt="Beetroot Birthday Party">
                                     <div class="carousel-caption d-none d-md-block">
                                         <h5>3 Years, 2015</h5>
                                     </div>
                                 </div>
                                 <div class="carousel-item">
-                                    <img src="{{ asset('images/gallery/birthdays/4_years_2016.jpg') }}" class="d-block w-100" alt="Beetroot Birthday Party">
+                                    <img src="{{ asset('images/gallery/4_years_2016.jpg') }}" class="d-block w-100" alt="Beetroot Birthday Party">
                                     <div class="carousel-caption d-none d-md-block">
                                         <h5>4 Years, 2016</h5>
                                     </div>
                                 </div>
                                 <div class="carousel-item">
-                                    <img src="{{ asset('images/gallery/birthdays/5_years_2017.jpg') }}" class="d-block w-100" alt="Beetroot Birthday Party">
+                                    <img src="{{ asset('images/gallery/5_years_2017.jpg') }}" class="d-block w-100" alt="Beetroot Birthday Party">
                                     <div class="carousel-caption d-none d-md-block">
                                         <h5>5 Years, 2017</h5>
                                     </div>
                                 </div>
                                 <div class="carousel-item">
-                                    <img src="{{ asset('images/gallery/birthdays/6_years_2018.jpg') }}" class="d-block w-100" alt="Beetroot Birthday Party">
+                                    <img src="{{ asset('images/gallery/6_years_2018.jpg') }}" class="d-block w-100" alt="Beetroot Birthday Party">
                                     <div class="carousel-caption d-none d-md-block">
                                         <h5>6 Years, 2018</h5>
                                     </div>
