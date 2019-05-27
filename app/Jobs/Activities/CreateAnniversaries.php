a747bf231a43527eeba5ae8fba265c591535a27f
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2019-05-27 23:57:13 +0300

    Cleanup

--- app/Jobs/Activities/CreateAnniversaries.php
+++ app/Jobs/Activities/CreateAnniversaries.php
@@ -5,14 +5,11 @@ namespace App\Jobs\Activities;
 use App\User;
 use App\Activity;
 use Illuminate\Bus\Queueable;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class CreateAnniversaries implements ShouldQueue
+class CreateAnniversaries
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, Queueable;
 
     /**
      * Execute the job.
